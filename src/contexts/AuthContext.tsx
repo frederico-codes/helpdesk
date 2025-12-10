f84bedf8e016f62034d8ac29a3d87ff26148a14a
frederico nakajima <fredericonakajimanakajima@gmail.com>
2025-12-10 18:01:04 -0300

    token no localstorage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -8,23 +8,41 @@ type AuthContextType = {
   save: (data: UserApiResponse) => void
 }
 
+const LOCAL_STORAGE_KEY = "@helpdesk"
+
 export const AuthContext = createContext({} as AuthContextType)
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [session, setSession] = useState<Session | null>(null)
 
   function save(data: UserApiResponse) {
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:user`, JSON.stringify(data.userWithoutPassword))
+    localStorage.setItem(`${ LOCAL_STORAGE_KEY}:token`, data.token)
+
     const sessionFormatted: Session = {
       token: data.token,
       user: data.userWithoutPassword,
-    }
+    }    
 
     setSession(sessionFormatted)
   }
 
+  function loadUser(){
+    const user = localStorage.getItem(`${LOCAL_STORAGE_KEY}:user`)
+    const token = localStorage.getItem(`${ LOCAL_STORAGE_KEY}:token`)
+
+    if(token && user){
+      setSession({
+        token,
+        user:JSON.parse(user)
+      })
+    }
+  }
+
   useEffect(() => {
-    console.log("🔁 SESSION ATUALIZADA:", session)
-  }, [session])
+    loadUser()
+  }, [])
+
 
   return (
     <AuthContext.Provider value={{ session, save }}>
